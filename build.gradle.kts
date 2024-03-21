eb4d2ec097b8972844d99dc9e16c813a795706b0
Vegard Veiset <veiset@gmail.com>
2024-03-21 17:23:32 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 	implementation("org.flywaydb:flyway-core:9.8.1")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	runtimeOnly("com.h2database:h2")
+	implementation("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
